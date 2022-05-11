b2746a9e375822c79838d82b5fd786e38f8663f7
bayezid mir <warner.roman@gmail.com>
2022-05-11 09:27:57 +0600

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Home from "./pages/Home/Home";
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./pages/Shared/Navbar";
+import About from "./pages/About/About";
 
 function App() {
   return (
     <div className="App">
-      <h2>Hello World!</h2>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h2 class="text-lime-600">This is Home</h2>
+    </div>
+  );
+};
+
+export default Home;
